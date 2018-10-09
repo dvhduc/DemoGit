f0574833c5eea76fe179aee04523125b77570f0e
Hong Duc <01627dlm@gmail.com>
2018-10-09 13:57:00 +0700

    Tao nut cong

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,33 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnadd
             // 
-            this.button1.Location = new System.Drawing.Point(67, 63);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 55);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "TestGit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnadd.Location = new System.Drawing.Point(61, 159);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(145, 55);
+            this.btnadd.TabIndex = 0;
+            this.btnadd.Text = "Cộng";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(51, 38);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(201, 22);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(51, 81);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(201, 22);
+            this.txt2.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 255);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnadd);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
     }
 }
 
